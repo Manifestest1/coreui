e4630e401b5d8b326a10c4ffa28ec9b711b283fa
Manifestest1 <manifestest1@gmail.com>
2024-04-18 09:29:07 +0530

    'changes'

--- database/migrations/2024_04_17_073440_contacts.php
+++ database/migrations/2024_04_17_073440_contacts.php
@@ -14,10 +14,11 @@ return new class extends Migration
         Schema::create('contacts', function (Blueprint $table) 
         {
             $table->id();
-            $table->string('name');
+            $table->string('name'); 
             $table->string('email');
             $table->string('subject');
             $table->string('message');
+            $table->timestamps();
         });
     }
 
